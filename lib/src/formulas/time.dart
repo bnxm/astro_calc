945dfe1b5573d3b3e6b2313c86be1f91be11d596
BendixMa <43601200+BendixMa@users.noreply.github.com>
2020-04-12 14:42:56 +0200

    c

--- lib/src/formulas/time.dart
+++ lib/src/formulas/time.dart
@@ -15,10 +15,11 @@ num solarTransitJ(num ds, num M, num L) {
 }
 
 num hourAngle(num h, num phi, num d) {
-  return math.acos((math.sin(h) - math.sin(phi) * math.sin(d)) / (math.cos(phi) * math.cos(d)));
+  final arc = (math.sin(h) - math.sin(phi) * math.sin(d)) / (math.cos(phi) * math.cos(d));
+  return math.acos(arc.clamp(-1.0, 1.0));
 }
 
-num getSetJ(num h, num  lw, num phi, num dec, num n, num M, num L) {
+num getSetJ(num h, num lw, num phi, num dec, num n, num M, num L) {
   final w = hourAngle(h, phi, dec);
   final a = approxTransit(w, lw, n);
 
